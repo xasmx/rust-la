0b1d1d750bfc9b72714d095430a54efa77c556c8
Sami Nopanen <snopanen@gmail.com>
2013-09-03 20:19:05 -0400

    More documentation for LU decomposition.

--- src/decomp/lu.rs
+++ src/decomp/lu.rs
@@ -4,12 +4,6 @@ use std::num::{One, Zero};
 use super::super::matrix::*;
 use super::super::util::{alloc_dirty_vec};
 
-pub struct LUDecomposition<T> {
-  lu : Matrix<T>,
-  pospivsign : bool,
-  piv : ~[uint]
-}
-
 // Ported from JAMA.
 // LU Decomposition.
 //
@@ -22,6 +16,87 @@ pub struct LUDecomposition<T> {
 // singular. The primary use of the LU decomposition is in the solution of
 // square systems of simultaneous linear equations. This will fail if the
 // matrix is singular.
+//
+// LU Decomposition:
+// A = LU,
+//   where L is a lower triangular matrix
+//   and U is an upper triangular matrix.
+//
+// Step 1: Set diagonals of L to 1:
+// Step 2: Solve columns of L and U in order from left to right.
+//         For a specific column, solve in order from top to bottom, first solving the column for U and then for L.
+//         For solving element e[i][j], write out the dot between i:th row of L and j:th column of U. By following
+//         this specific order, you'll notice that by the time you are solving for that specific element, you have
+//         already solved all the other variables in the equation, thus you just need to plug in the numbers to
+//         solve the element.
+//
+//  Example: 3x3 matrix LU decomposition:
+//    l11   0   0   u11 u12 u13   a11 a12 a13
+//    l21 l22   0 *   0 u22 u23 = a21 a22 a23
+//    l31 l32 l33     0   0 u33   a31 a32 a33
+//  
+//  Step 1:
+//    l11 = 1
+//    l22 = 1
+//    l33 = 1
+//
+//  Step 2:
+//    u11 = a11 / l11
+//    l21 = a21 / u11
+//    l31 = a31 / u11
+//
+//    u12 = a12 / l11
+//    u22 = (a22 - u12 * l21) / l22
+//    l32 = (a32 - l31 * u12) / u22
+//
+//    u13 = a13 / l11
+//    u23 = (a23 - u13 * l21) / l22
+//    u33 = (a33 - u13 * l31 - u23 * l32) / l33
+//
+//  In general, from above we can derive the following equations for solving the specific elements:
+//  l[i][i] = 1
+//  
+//  u[i][j] = 1 / l[i][i] * (a[i][j] - <l[0 .. (i - 1)][:], u[:][0 .. (i - 1)]>)
+//          = a[i][j] - <l[0 .. (i - 1)][:], u[:][0 .. (i - 1)]>                  , (as l[i][i] = 1).
+//  
+//  l[i][j] = 1 / u[j][j] * (a[i][j] - <l[0 .. (j - 1)][:], u[:][0 .. (j - 1)]>)
+//
+//  The above algorithm is generally known as Crout's algorithm.
+//
+//  Examining the above equations closely, we can determine that the algorithm works as long as u[j][j]
+//  is not zero (which would cause a division by zero). We can generalize this by adding a permutation
+//  matrix P, giving us LUP decomposition, which always exists:
+//
+//  PA = LU,
+//    where P is a permutation matrix,
+//    L is a lower triangular matrix
+//    and U is an upper triangular matrix.
+//
+//  Applying pivoting also makes the algorithm numerically stable. We can change the algorithm to do
+//  (partial) pivoting in the following way:
+//    1. When calculating l[i][j], do not perform the division with u[j][j].
+//    2. Once you have calculated all the values for the column j, find the maximum value of lu[i..(n-1)][j].
+//    3. Swap the pivot row with the row with the maximum value, so that the maximum value is at the pivot row.
+//    4. Perform the division steps of the original algorithm with the new value of u[j][j] (the maximum).
+//
+//  LUP decomposition can be used to solve a set of linear equations 'Ax = b' in the following manner
+//  (assuming A is invertible):
+//    Ax = b
+//    PAx = Pb
+//    LUx = Pb
+//    (LU)^-1 LUx = (LU)^-1 Pb
+//    x = (LU)^-1 Pb
+//      = U^-1 L^-1 Pb
+//
+
+pub struct LUDecomposition<T> {
+  // U is stored in diagonals and above. Elements below diagonals are zero for U.
+  // L is stored below diagonals. Diagonal elements are one for U and elements above diagonals are zero.
+  lu : Matrix<T>,
+  pospivsign : bool,
+  piv : ~[uint]
+}
+
 impl<T : Add<T, T> + Sub<T, T> + Mul<T, T> + Div<T, T> + Neg<T> + Eq + Ord + ApproxEq<T> + One + Zero + Clone + Signed> LUDecomposition<T> {
   pub fn new(a : &Matrix<T>) -> LUDecomposition<T> {
     // Use a "left-looking", dot-product, Crout/Doolittle algorithm.
@@ -36,8 +111,10 @@ impl<T : Add<T, T> + Sub<T, T> + Mul<T, T> + Div<T, T> + Neg<T> + Eq + Ord + App
     let mut pospivsign = true;
     let mut lucolj = alloc_dirty_vec(m as uint);
 
-    // Outer loop.
+    // Solve columns 0..(n-1) of L and U in order.
     for j in range(0, n) {
+      // Solve column j of L and U.
+
       // Make a copy of the j-th column to localize references.
       for i in range(0, m) {
         lucolj[i] = ludata[i * n + j].clone();
@@ -109,6 +186,7 @@ impl<T : Add<T, T> + Sub<T, T> + Mul<T, T> + Div<T, T> + Neg<T> + Eq + Ord + App
   }
 
   pub fn get_l(&self) -> Matrix<T> {
+    // L is stored below diagonals. Diagonal elements are one for U and elements above diagonals are zero.
     let m = self.lu.noRows;
     let n = if self.lu.noRows >= self.lu.noCols { self.lu.noCols } else { self.lu.noRows };
     let mut ldata = alloc_dirty_vec(m * n);
@@ -127,6 +205,7 @@ impl<T : Add<T, T> + Sub<T, T> + Mul<T, T> + Div<T, T> + Neg<T> + Eq + Ord + App
   }
 
   pub fn get_u(&self) -> Matrix<T> {
+    // U is stored in diagonals and above. Elements below diagonals are zero for U.
     let m = if self.lu.noRows >= self.lu.noCols { self.lu.noCols as int } else { self.lu.noRows as int };
     let n = self.lu.noCols as int;
     let mut udata = alloc_dirty_vec((m * n) as uint);
