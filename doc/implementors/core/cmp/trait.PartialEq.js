cf0968982a31d82bec2ecf6ca8331104562af4f8
Sami Nopanen <snopanen@gmail.com>
2014-06-16 15:37:17 -0400

    Initial commit of docs.

---
+++ doc/implementors/core/cmp/trait.PartialEq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['la'] = ["impl&lt;T: <a class='trait' href='http://doc.rust-lang.org/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a>&gt; <a class='trait' href='http://doc.rust-lang.org/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='struct' href='la/struct.Matrix.html' title='la::Matrix'>Matrix</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
