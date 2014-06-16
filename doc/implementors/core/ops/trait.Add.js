1d10d627f7b194312d80c9444d1d521e4c4f0363
Sami Nopanen <snopanen@gmail.com>
2014-06-16 17:50:13 -0400

    Update docs.

--- doc/implementors/core/ops/trait.Add.js
+++ doc/implementors/core/ops/trait.Add.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['la'] = ["impl&lt;T: <a class='trait' href='http://doc.rust-lang.org/core/ops/trait.Add.html' title='core::ops::Add'>Add</a>&lt;T, T&gt;&gt; <a class='trait' href='http://doc.rust-lang.org/core/ops/trait.Add.html' title='core::ops::Add'>Add</a>&lt;<a class='struct' href='la/struct.Matrix.html' title='la::Matrix'>Matrix</a>&lt;T&gt;, <a class='struct' href='la/struct.Matrix.html' title='la::Matrix'>Matrix</a>&lt;T&gt;&gt; for <a class='struct' href='la/struct.Matrix.html' title='la::Matrix'>Matrix</a>&lt;T&gt;",];
+implementors['la'] = ["impl&lt;T: <a class='trait' href='http://doc.rust-lang.org/core/num/trait.Num.html' title='core::num::Num'>Num</a>&gt; <a class='trait' href='http://doc.rust-lang.org/core/ops/trait.Add.html' title='core::ops::Add'>Add</a>&lt;<a class='struct' href='la/struct.Matrix.html' title='la::Matrix'>Matrix</a>&lt;T&gt;, <a class='struct' href='la/struct.Matrix.html' title='la::Matrix'>Matrix</a>&lt;T&gt;&gt; for <a class='struct' href='la/struct.Matrix.html' title='la::Matrix'>Matrix</a>&lt;T&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
