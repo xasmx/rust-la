fee7e69f56268dfcabf0e8d497bb800fc180a8cf
Sami Nopanen <snopanen@gmail.com>
2014-06-19 22:51:04 -0400

    Update docs.

---
+++ doc/implementors/core/ops/trait.BitOr.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['la'] = ["impl&lt;T: <a class='trait' href='http://doc.rust-lang.org/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a>&gt; <a class='trait' href='http://doc.rust-lang.org/core/ops/trait.BitOr.html' title='core::ops::BitOr'>BitOr</a>&lt;<a class='struct' href='la/struct.Matrix.html' title='la::Matrix'>Matrix</a>&lt;T&gt;, <a class='struct' href='la/struct.Matrix.html' title='la::Matrix'>Matrix</a>&lt;T&gt;&gt; for <a class='struct' href='la/struct.Matrix.html' title='la::Matrix'>Matrix</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
