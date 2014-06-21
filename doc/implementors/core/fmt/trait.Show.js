b61b6d6478b4abe92e502d2b26108072b16a2f8f
Sami Nopanen <snopanen@gmail.com>
2014-06-21 00:13:08 -0400

    Update docs.

---
+++ doc/implementors/core/fmt/trait.Show.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['la'] = ["impl&lt;T: <a class='trait' href='http://doc.rust-lang.org/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a>&gt; <a class='trait' href='http://doc.rust-lang.org/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='struct' href='la/struct.Matrix.html' title='la::Matrix'>Matrix</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
