b7ac6457d02b841d4b7407dbe713920bddf75009
Sami Nopanen <snopanen@gmail.com>
2015-11-08 13:33:20 -0500

    Indent fixes.

--- src/approxeq.rs
+++ src/approxeq.rs
@@ -10,38 +10,38 @@
 
 /// Trait for testing approximate equality
 pub trait ApproxEq<Eps> {
-    fn approx_epsilon() -> Eps;
-    fn approx_eq(&self, other: &Self) -> bool;
-    fn approx_eq_eps(&self, other: &Self, approx_epsilon: &Eps) -> bool;
+  fn approx_epsilon() -> Eps;
+  fn approx_eq(&self, other: &Self) -> bool;
+  fn approx_eq_eps(&self, other: &Self, approx_epsilon: &Eps) -> bool;
 }
 
 impl ApproxEq<f32> for f32 {
-    #[inline]
-    fn approx_epsilon() -> f32 { 1.0e-6 }
+  #[inline]
+  fn approx_epsilon() -> f32 { 1.0e-6 }
 
-    #[inline]
-    fn approx_eq(&self, other: &f32) -> bool {
-       self.approx_eq_eps(other, &1.0e-6)
-    }
+  #[inline]
+  fn approx_eq(&self, other: &f32) -> bool {
+    self.approx_eq_eps(other, &1.0e-6)
+  }
 
-    #[inline]
-    fn approx_eq_eps(&self, other: &f32, approx_epsilon: &f32) -> bool {
-       (*self - *other).abs() < *approx_epsilon
-    }
+  #[inline]
+  fn approx_eq_eps(&self, other: &f32, approx_epsilon: &f32) -> bool {
+    (*self - *other).abs() < *approx_epsilon
+  }
 }
 
 
 impl ApproxEq<f64> for f64 {
-    #[inline]
-    fn approx_epsilon() -> f64 { 1.0e-6 }
+  #[inline]
+  fn approx_epsilon() -> f64 { 1.0e-6 }
 
-    #[inline]
-    fn approx_eq(&self, other: &f64) -> bool {
-        self.approx_eq_eps(other, &1.0e-6)
-    }
+  #[inline]
+  fn approx_eq(&self, other: &f64) -> bool {
+    self.approx_eq_eps(other, &1.0e-6)
+  }
     
-    #[inline]
-    fn approx_eq_eps(&self, other: &f64, approx_epsilon: &f64) -> bool {
-        (*self - *other).abs() < *approx_epsilon
-    }
+  #[inline]
+  fn approx_eq_eps(&self, other: &f64, approx_epsilon: &f64) -> bool {
+    (*self - *other).abs() < *approx_epsilon
+  }
 }
